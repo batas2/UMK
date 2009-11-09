4b561f2cf4f9acceb5bfac74be1607603b8f1135
Bartek <batas2@interia.pl>
2009-11-09 12:45:25 +0100

    04 week - 10 file added

--- PSI/10_dd_batas2.c
+++ PSI/10_dd_batas2.c
@@ -3,8 +3,15 @@
 #include<signal.h>
 #include<unistd.h>
 
-int _stop = 0, _list = 0;
+/*
+ * Zmienne globalne oznaczajace czy zatrzymac kopiowanie 
+ * oraz czy wyswietlic postepo
+ */
+int _stop = 0, _list = 0; 
 
+/*
+ * funkcja wywolana po otrzymaniu sygnalu SIGUSR1
+ */
 void list(int v)
 {
 
@@ -12,11 +19,17 @@ void list(int v)
 	signal(SIGUSR1, list);
 }
 
+/*
+ * funkcja wywolywana po otrzymaniu sygnalu SUGUSR2
+ */
 void stop(int v)
 {
 	_stop = 1;
 }
 
+/*
+ * Funkcj kopiujaca plik uzywajaca strumieni danych
+ */
 int copy(const char *src_path, const char *dest_path,
 	 const int block_count, const int block_size)
 {
@@ -25,13 +38,13 @@ int copy(const char *src_path, const char *dest_path,
 	int	i = 0;
 	char	*buf;
 	
-	src_file	= fopen(src_path, "r");
-	dest_file	= fopen(dest_path, "w");
-	buf		= (char *)malloc(sizeof(char) * block_size);
+	src_file	= fopen(src_path, "r"); //Otwieram strumien do czytania
+	dest_file	= fopen(dest_path, "w"); //Otwieram strumien do pisania
+	buf		= (char *)malloc(sizeof(char) * block_size); //blok danych
 
 	while( !feof(src_file) && (i++ < block_count) && !_stop ){
-		fread(buf, sizeof(char), sizeof(char) * block_size, src_file);
-		fwrite(buf, sizeof(char), sizeof(char) * block_size, dest_file);
+		fread(buf, sizeof(char), block_size, src_file); //czytam blok danych
+		fwrite(buf, sizeof(char), block_size, dest_file); //pisze blok danych
 		if(_list){
 			printf("Przeslano: %dB\n", i * block_size);
 			_list = 0;
@@ -39,13 +52,13 @@ int copy(const char *src_path, const char *dest_path,
 //		sleep(2);
 	}
 	
-	fflush(dest_file);
+	fflush(dest_file); //Tak na wszelki wypadek bufor strumienia zapisuje do pliku
 	
 	fclose(src_file);
 	fclose(dest_file);
 	
 	if(_stop){
-		remove(dest_path);
+		remove(dest_path); //usuwam plik
 	}
 			
 }
@@ -54,11 +67,16 @@ int main(int argc, char *argv[])
 {
 	int	block_count = 0, block_size = 0;
 
-	block_count = atoi(argv[3]);
-	block_size = atoi(argv[4]);
+	if(argc == 5){
+		block_count = atoi(argv[3]);
+		block_size = atoi(argv[4]);
 
-	signal(SIGUSR1, list);
-	signal(SIGUSR2, stop);
+		//rejestruje funkcje do przechwytywania sygnalow
+		signal(SIGUSR1, list); 
+		signal(SIGUSR2, stop);
 
-	copy(argv[1], argv[2], block_count, block_size);
+		copy(argv[1], argv[2], block_count, block_size);
+	}else{
+		printf("Nie poprawne parametry!\nmojedd plik_wej plik_wyj ilosc_blokow rozmiar_bloku\n");
+	}
 }
