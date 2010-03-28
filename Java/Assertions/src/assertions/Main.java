f98f37c75c1dac021fb3cb544f183aac07fac78c
batas2 <batas2@interia.pl>
2010-03-28 23:06:57 +0200

    asserions example - fist example

--- Java/Assertions/src/assertions/Main.java
+++ Java/Assertions/src/assertions/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package assertions;
 
 /**
@@ -11,11 +10,21 @@ package assertions;
  */
 public class Main {
 
+    static void asserion1(int v) {
+        if (v % 3 == 0) {
+            System.out.println("Reszta z dzielnia: 0");
+        } else if (v % 3 == 1) {
+            System.out.println("Reszta z dzielnia: 1");
+        } else {
+            assert v % 4 == 2 : v;
+            System.out.println("Reszta z dzielnia: 2");
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        asserion1(5);
     }
-
 }
