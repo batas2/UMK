930d962a9921cd4420b1113456b9d4a6c6f4e623
batas2 <batas2@interia.pl>
2010-03-29 12:05:56 +0200

    asserions example - fist part of examples

--- Java/Assertions/src/assertions/Main.java
+++ Java/Assertions/src/assertions/Main.java
@@ -21,7 +21,7 @@ public class Main {
         } else if (v % 3 == 1) {
             System.out.println("Reszta z dzielnia: 1");
         } else {
-            assert v % 4 == 2 : v;
+            assert v % 3 == 2 : v;
             System.out.println("Reszta z dzielnia: 2");
         }
     }
