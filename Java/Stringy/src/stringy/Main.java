36046fdf25ea9cb6d991df8ad8d79059cb80067d
batas2 <batas2@interia.pl>
2010-03-29 18:15:18 +0200

    Stringy v0.5

--- Java/Stringy/src/stringy/Main.java
+++ Java/Stringy/src/stringy/Main.java
@@ -1,5 +1,8 @@
 package stringy;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author batas2
@@ -9,8 +12,61 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void Iniclay() {
+        String s = new String();
+        s = "Bartosz Frackowiak";
+        String tab[] = new String[2];
+        tab = s.split(" ");
+
+        System.out.println(String.valueOf(tab[0].charAt(0)) + String.valueOf(tab[1].charAt(0)));
     }
 
+    public static void Konkatenacja() {
+        String a = "abc";
+        String b = "def";
+        System.out.println(a + b + 43 + 27);
+        System.out.println(26 + a + 43 + b);
+        System.out.println(26 + 43 + a + b);
+    }
+
+    public static void Anagram() {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = new String();
+        String str2 = new String();
+
+        try {
+            str1 = in.readLine();
+            str2 = in.readLine();
+        } catch (IOException e) {
+            System.out.print(e);
+        }
+
+        char[] str1c = str1.toCharArray();
+        char[] str2c = str2.toCharArray();
+
+        Arrays.sort(str1c);
+        Arrays.sort(str2c);
+        boolean sa = true;
+        if (str1c.length == str2c.length) {
+            for (int i = 0; i < str1c.length; i++) {
+                if ((str1c[i] != str2c[i]) && sa) {
+                    System.out.println("Nie sa anagramami");
+                    sa = false;
+                }
+            }
+            if (sa) {
+                System.out.println("sa anagramami");
+            }
+        } else {
+            System.out.println("Nie sa anagramami");
+        }
+    }
+
+    public static void main(String[] args) {
+        Iniclay();
+        Konkatenacja();
+        Anagram();
+    }
 }
