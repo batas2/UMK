f7b2c6636c5604c736aab6968ef303b735a136ff
batas2 <batas2@interia.pl>
2010-04-01 19:47:59 +0200

    09_Opacity

---
+++ LGM/09_Opacity/MyWidget.h
@@ -0,0 +1,45 @@
+/*
+ * File:   MyWidget.h
+ * Author: batas2
+ *
+ * Created on 1 kwiecień 2010, 18:02
+ */
+
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QComboBox>
+#include <QtGui/QGroupBox>
+#include <QtGui/QSlider>
+#include <QtGui/QWidget>
+#include <QtGui/QPainter>
+
+#ifndef _MYWIDGET_H
+#define	_MYWIDGET_H
+
+class MyWidget : public QWidget {
+    Q_OBJECT
+private:
+    int _picWidth;
+    int _picHeight;
+    int _selectedFunc;
+
+    QImage *_image, *_imageA, *_imageB;
+
+    QGroupBox *_groupBox;
+    QComboBox *_comboBox;
+    QSlider *_slider;
+
+    void createImage(double alpha);
+    void paintEvent(QPaintEvent *e);
+
+public:
+    MyWidget(int Width, int Height, QWidget *parent = 0);
+
+public slots:
+    void setSliderValue(int);
+    void setComboValue(int);
+
+};
+
+#endif	/* _MYWIDGET_H */
+
