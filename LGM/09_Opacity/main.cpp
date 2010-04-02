081a1003ce6b4d134e65ccd17e3a46c58df9b7ff
batas2 <batas2@interia.pl>
2010-04-02 12:59:08 +0200

    09_Opacity

--- LGM/09_Opacity/main.cpp
+++ LGM/09_Opacity/main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    MyWidget widget(400, 350);
+    MyWidget widget(400, 400);
     widget.show();
     return app.exec();
 }
