fb568d9233ee9ccf05594f1632889a23d9c2dcc7
batas2 <batas2@interia.pl>
2010-04-08 10:01:21 +0200

    10_przeksztalcenia

---
+++ LGM/10_przeksztalcenia/Point.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Point.h
+ * Author: batas2
+ *
+ * Created on 7 kwiecień 2010, 13:24
+ */
+
+#ifndef _POINT_H
+#define	_POINT_H
+
+class Point {
+public:
+    int X, Y;
+    int R, G, B;
+
+    Point();
+    Point(int X, int Y, int R = 0, int G = 0, int B = 0);
+};
+
+#endif	/* _POINT_H */
+
