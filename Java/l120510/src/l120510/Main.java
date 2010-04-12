6bde8cc67618151150488fc0d68f7592b9733e37
batas2 <batas2@interia.pl>
2010-04-12 16:23:01 +0200

    10_przeksztalcenia

---
+++ Java/l120510/src/l120510/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l120510;
+
+/**
+ *
+ * @author batas2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
