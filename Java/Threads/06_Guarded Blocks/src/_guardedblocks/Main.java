8bfe641336c6324686cbe472c260d25c19b187c8
batas2 <batas2@interia.pl>
2010-04-26 14:45:18 +0200

    Threads

---
+++ Java/Threads/06_Guarded Blocks/src/_guardedblocks/Main.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ */
+package _guardedblocks;
+
+/**
+ *
+ * @author batas2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
