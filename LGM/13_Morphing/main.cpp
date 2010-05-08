b5a66d764ccbdfd0c6bff1c8550b9ecdbbb4a586
batas2 <batas2@interia.pl>
2010-05-08 17:15:30 +0200

    13_

---
+++ LGM/13_Morphing/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: batas2, http://www.batas2.boo.pl/
+ *
+ * Created on 5 maj 2010, 16:16
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
