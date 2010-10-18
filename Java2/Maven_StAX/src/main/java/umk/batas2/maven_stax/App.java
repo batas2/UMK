13cd8b01cd575e0a12ed7c671e7ca446cb7aadb3
Bartosz Frąckowiak <batas2@interia.pl>
2010-10-18 23:59:28 +0200

    Maven_StAX

---
+++ Java2/Maven_StAX/src/main/java/umk/batas2/maven_stax/App.java
@@ -0,0 +1,13 @@
+package umk.batas2.maven_stax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
